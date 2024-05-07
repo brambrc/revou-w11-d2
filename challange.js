04bff1c424f40d898d6ef683a94112e3264d8f43
brambrc <anggiet.bracmatya@gmail.com>
2024-05-07 20:46:31 +0700

    answer

--- challange.js
+++ challange.js
@@ -1,28 +1,41 @@
-// Program untuk mencari bilangan prima di antara dua angka
 const readline = require('readline');
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-})
+});
 
 rl.question("Masukkan batas bawah: ", (batasBawah) => {
 
     rl.question("Masukkan batas atas: ", (batasAtas) => {
         
-        findPrimeNumbers(batasBawah, batasAtas);
+        // Panggil fungsi untuk mencari dan mencetak bilangan prima
+        findPrimeNumbers(parseInt(batasBawah), parseInt(batasAtas));
         rl.close();
         
     });
 });
-console.log("Bilangan prima di antara " + batasBawah + " dan " + batasAtas + ":");
 
 // Fungsi untuk mengecek apakah sebuah bilangan prima
 function isPrime(number) {
-    
+    if (number <= 1) {
+        return false;
+    }
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+        if (number % i === 0) {
+            return false;
+        }
+    }
+    return true;
 }
 
-// Loop untuk mencari bilangan prima dan mencetaknya
-for (let i = batasBawah; i <= batasAtas; i++) {
-    
+// Fungsi untuk mencari bilangan prima di antara batas bawah dan batas atas, lalu mencetaknya
+function findPrimeNumbers(batasBawah, batasAtas) {
+    console.log("Bilangan prima di antara " + batasBawah + " dan " + batasAtas + ":");
+
+    for (let i = batasBawah; i <= batasAtas; i++) {
+        if (isPrime(i)) {
+            console.log(i);
+        }
+    }
 }

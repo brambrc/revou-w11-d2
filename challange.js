e84fd6dd62fa6fc3a8ce74f2d9b94cea06495f0c
brambrc <anggiet.bracmatya@gmail.com>
2024-05-07 20:37:48 +0700

    added readline

--- challange.js
+++ challange.js
@@ -1,7 +1,20 @@
 // Program untuk mencari bilangan prima di antara dua angka
-const batasBawah = parseInt(prompt("Masukkan batas bawah:"));
-const batasAtas = parseInt(prompt("Masukkan batas atas:"));
+const readline = require('readline');
 
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+rl.question("Masukkan batas bawah: ", (batasBawah) => {
+
+    rl.question("Masukkan batas atas: ", (batasAtas) => {
+        
+        findPrimeNumbers(batasBawah, batasAtas);
+        rl.close();
+        
+    });
+});
 console.log("Bilangan prima di antara " + batasBawah + " dan " + batasAtas + ":");
 
 // Fungsi untuk mengecek apakah sebuah bilangan prima

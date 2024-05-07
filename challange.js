80fc0441c8191a538c2fb23d66daab5f559c0a5b
brambrc <anggiet.bracmatya@gmail.com>
2024-05-07 20:32:01 +0700

    added challange

---
+++ challange.js
@@ -0,0 +1,15 @@
+// Program untuk mencari bilangan prima di antara dua angka
+const batasBawah = parseInt(prompt("Masukkan batas bawah:"));
+const batasAtas = parseInt(prompt("Masukkan batas atas:"));
+
+console.log("Bilangan prima di antara " + batasBawah + " dan " + batasAtas + ":");
+
+// Fungsi untuk mengecek apakah sebuah bilangan prima
+function isPrime(number) {
+    
+}
+
+// Loop untuk mencari bilangan prima dan mencetaknya
+for (let i = batasBawah; i <= batasAtas; i++) {
+    
+}
--- while-a.js
+++ while-a.js
@@ -1,26 +1,51 @@
-//
 const readline = require('readline');
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-})
+});
 
+rl.question("Masukkan bilangan yang ingin dipangkatkan: ", (bilangan) => {
+    const angka = parseInt(bilangan);
+    rl.question("Masukkan pangkat: ", (pangkat) => {
+        const pangkatAngka = parseInt(pangkat);
+        
+        let hasil = 1;
+        let counter = 0;
 
-rl.question("Masukkan angka batas:", (angkaBatas) => {
-    const batas = parseInt(angkaBatas);
-    let fib1 = 0;
-    let fib2 = 1;
-    let hasil = 0;
+        console.log(`Hasil ${angka} pangkat ${pangkatAngka} adalah:`);
+        
+        while (counter < pangkatAngka) {
+            // hasil *= angka;
+            hasil = hasil * angka;
+            counter++;
+        }
 
-    while (hasil <= batas) {
-        hasil = fib1 + fib2;
-        fib1 = fib2;
-        fib2 = hasil;
-    }
+        console.log(hasil);
+        rl.close();
+    });
+});
 
-    console.log("Bilangan Fibonacci terkecil lebih besar dari " + batas + " adalah: " + hasil);
-    rl.close();
+//Kondisi Start Program # Loop Pertama
+// hasil = 1, counter = 0, angka = 2, pangkatAngka = 3
+// counter < pangkatAngka -> 0 < 3 | TRUE
+// hasil = 1 * 2 = 2, 
+// counter = 1
 
-}
-);
+//Kondisi Loop #2
+// hasil = 2, counter = 1, angka = 2, pangkatAngka = 3
+// counter < pangkatAngka -> 1 < 3 | TRUE
+// hasil = 2 * 2 = 4,
+// counter = 2
+
+//Kondisi Loop #3
+// hasil = 4, counter = 2, angka = 2, pangkatAngka = 3
+// counter < pangkatAngka -> 2 < 3 | TRUE
+// hasil = 4 * 2 = 8,
+// counter = 3
+
+//Kondisi Loop #4
+// hasil = 8, counter = 3, angka = 2, pangkatAngka = 3
+// counter < pangkatAngka -> 3 < 3 | FALSE
+// Keluar dari Loop
+// Output hasil = 8

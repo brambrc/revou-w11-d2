3df50e2c78f406a100aebcf4f79f8404faccd5d5
brambrc <anggiet.bracmatya@gmail.com>
2024-05-07 20:20:46 +0700

    update while-do

--- while-a.js
+++ while-a.js
@@ -1,13 +1,26 @@
-// Program untuk menemukan bilangan Fibonacci terkecil lebih besar dari angka tertentu
-const batas = parseInt(prompt("Masukkan angka batas:"));
-let fib1 = 0;
-let fib2 = 1;
-let hasil = 0;
+//
+const readline = require('readline');
 
-while (hasil <= batas) {
-    hasil = fib1 + fib2;
-    fib1 = fib2;
-    fib2 = hasil;
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+
+rl.question("Masukkan angka batas:", (angkaBatas) => {
+    const batas = parseInt(angkaBatas);
+    let fib1 = 0;
+    let fib2 = 1;
+    let hasil = 0;
+
+    while (hasil <= batas) {
+        hasil = fib1 + fib2;
+        fib1 = fib2;
+        fib2 = hasil;
+    }
+
+    console.log("Bilangan Fibonacci terkecil lebih besar dari " + batas + " adalah: " + hasil);
+    rl.close();
+
 }
-
-console.log("Bilangan Fibonacci terkecil lebih besar dari " + batas + " adalah: " + hasil);
+);
